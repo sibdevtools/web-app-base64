235fac127ca8c6ba8b3cd7237b1fc64c2e15a2e4
sibmaks <sibmaks@vk.com>
2024-09-12 15:15:17 +0300

    Add .editorconfig

--- web-app/src/main/js/src/App.tsx
+++ web-app/src/main/js/src/App.tsx
@@ -1,38 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'hugeicons-react'
-import {DecodeBase64ToFile, DecodeBase64ToText, EncodeFileToBase64, EncodeTextToBase64} from "./componenets";
+import { DecodeBase64ToFile, DecodeBase64ToText, EncodeFileToBase64, EncodeTextToBase64 } from './componenets';
 
 const App: React.FC = () => {
-    const [mode, setMode] = useState('textToBase64');
-    const [encoding, setEncoding] = useState('UTF-8');
+  const [mode, setMode] = useState('textToBase64');
+  const [encoding, setEncoding] = useState('UTF-8');
 
-    const renderModeComponent = () => {
-        switch (mode) {
-            case 'textToBase64':
-                return <EncodeTextToBase64 encoding={encoding}/>;
-            case 'base64ToText':
-                return <DecodeBase64ToText encoding={encoding}/>;
-            case 'fileToBase64':
-                return <EncodeFileToBase64/>;
-            case 'base64ToFile':
-                return <DecodeBase64ToFile/>;
-            default:
-                return <div>Please select a mode</div>;
-        }
-    };
+  const renderModeComponent = () => {
+    switch (mode) {
+      case 'textToBase64':
+        return <EncodeTextToBase64 encoding={encoding} />;
+      case 'base64ToText':
+        return <DecodeBase64ToText encoding={encoding} />;
+      case 'fileToBase64':
+        return <EncodeFileToBase64 />;
+      case 'base64ToFile':
+        return <DecodeBase64ToFile />;
+      default:
+        return <div>Please select a mode</div>;
+    }
+  };
 
-    return (
-        <div className="container mt-5">
-            <select className="form-control form-select" value={mode} onChange={e => setMode(e.target.value)}>
-                <option value="textToBase64">Text to Base64 Text</option>
-                <option value="base64ToText">Base64 Text to Text</option>
-                <option value="fileToBase64">File to Base64 Text</option>
-                <option value="base64ToFile">Base64 Text to File</option>
-            </select>
-            {renderModeComponent()}
-        </div>
-    );
+  return (
+    <div className="container mt-5">
+      <label htmlFor="modeSelect">Mode Theme:</label>
+      <select
+        id="modeSelect"
+        className="form-control form-select"
+        value={mode} onChange={e => setMode(e.target.value)}>
+        <option value="textToBase64">Text to Base64 Text</option>
+        <option value="base64ToText">Base64 Text to Text</option>
+        <option value="fileToBase64">File to Base64 Text</option>
+        <option value="base64ToFile">Base64 Text to File</option>
+      </select>
+      {renderModeComponent()}
+    </div>
+  );
 };
 
 export default App;
--- web-app/src/main/js/src/componenets/DecodeBase64ToFile.tsx
+++ web-app/src/main/js/src/componenets/DecodeBase64ToFile.tsx
@@ -1,46 +1,46 @@
-import React, {useState} from 'react';
-import {base64ToFile} from '../utils/converters';
-import {Download04Icon} from "hugeicons-react";
+import React, { useState } from 'react';
+import { base64ToFile } from '../utils/converters';
+import { Download04Icon } from 'hugeicons-react';
 
 export const DecodeBase64ToFile = () => {
-    const [inputText, setInputText] = useState('');
-    const [filename, setFilename] = useState('download.txt');
+  const [inputText, setInputText] = useState('');
+  const [filename, setFilename] = useState('download.txt');
 
-    const handleConvert = () => {
-        base64ToFile(inputText, filename);
-    };
+  const handleConvert = () => {
+    base64ToFile(inputText, filename);
+  };
 
-    return (
-        <div className="container mt-5">
-            <div className="row">
-                <div className="mb-3">
-                    <label htmlFor="fileNameInput" className="form-label">Filename</label>
-                    <input id={"fileNameInput"}
-                           className="form-control"
-                           type="text"
-                           value={filename}
-                           placeholder="Filename"
-                           onChange={(e) => setFilename(e.target.value)}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="inputTextArea" className="form-label">Content</label>
-                    <textarea id={"inputTextArea"}
-                              className="form-control"
-                              value={inputText}
-                              placeholder="Base64"
-                              onChange={(e) => setInputText(e.target.value)}/>
-                </div>
-            </div>
-            <div className="row mt-3 text-center">
-                <div className="col">
-                    <button
-                        className="btn btn-primary"
-                        onClick={handleConvert}>
-                        <Download04Icon/>
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="mb-3">
+          <label htmlFor="fileNameInput" className="form-label">Filename</label>
+          <input id={'fileNameInput'}
+                 className="form-control"
+                 type="text"
+                 value={filename}
+                 placeholder="Filename"
+                 onChange={(e) => setFilename(e.target.value)} />
         </div>
-    );
+        <div className="mb-3">
+          <label htmlFor="inputTextArea" className="form-label">Content</label>
+          <textarea id={'inputTextArea'}
+                    className="form-control"
+                    value={inputText}
+                    placeholder="Base64"
+                    onChange={(e) => setInputText(e.target.value)} />
+        </div>
+      </div>
+      <div className="row mt-3 text-center">
+        <div className="col">
+          <button
+            className="btn btn-primary"
+            onClick={handleConvert}>
+            <Download04Icon />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
