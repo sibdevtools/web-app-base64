29e0705856655e799f56002728719297db3a2277
sibmaks <sibmaks@vk.com>
2024-09-13 13:27:30 +0300

    Add localizations

--- web-app/src/main/java/com/github/simplemocks/web/app/base64/WebAppBase64.java
+++ web-app/src/main/java/com/github/simplemocks/web/app/base64/WebAppBase64.java
@@ -2,6 +2,7 @@ package com.github.simplemocks.web.app.base64;
 
 import com.github.simple_mocks.localization_service.api.dto.LocalizationId;
 import com.github.simple_mocks.localization_service.api.dto.LocalizationSourceId;
+import com.github.simple_mocks.localization_service.mutable.api.source.LocalizationJsonSource;
 import com.github.simplemocks.webapp.api.dto.HealthStatus;
 import com.github.simplemocks.webapp.api.dto.WebApplication;
 import jakarta.annotation.Nonnull;
@@ -14,6 +15,16 @@ import java.util.Set;
  * @since 0.0.1
  */
 @Component
+@LocalizationJsonSource(
+        systemCode = "WEB.APP.BASE64",
+        path = "classpath:/web/app/base64/content/localizations/eng.json",
+        iso3Code = "eng"
+)
+@LocalizationJsonSource(
+        systemCode = "WEB.APP.BASE64",
+        path = "classpath:/web/app/base64/content/localizations/rus.json",
+        iso3Code = "rus"
+)
 public class WebAppBase64 implements WebApplication {
     private static final LocalizationSourceId LOCALIZATION_SOURCE_ID = new LocalizationSourceId("WEB.APP.BASE64");
 
@@ -32,7 +43,7 @@ public class WebAppBase64 implements WebApplication {
     @Nonnull
     @Override
     public LocalizationId getIconCode() {
-        return new LocalizationId(LOCALIZATION_SOURCE_ID, "web.app.base64");
+        return new LocalizationId(LOCALIZATION_SOURCE_ID, "web.app.base64.icon");
     }
 
     @Nonnull
