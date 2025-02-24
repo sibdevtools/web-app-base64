17aedc890cc6b197216581ffe81fb07c1aca7dbd
sibmaks <sibmaks@vk.com>
2025-02-24 11:10:54 +0300

    [Web App Base64] Show file body optionally resolves #8

--- web-app-frontend/src/App.tsx
+++ web-app-frontend/src/App.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'hugeicons-react'
-import { EncodeDecodeText, EncodeDecodeFile } from './componenets';
-import { Container, FormLabel, FormSelect } from 'react-bootstrap';
+import { EncodeDecodeFile, EncodeDecodeText } from './componenets';
+import { Col, Container, FormLabel, FormSelect, Row } from 'react-bootstrap';
+
+type Mode = 'text' | 'file';
 
 const App: React.FC = () => {
-  const [mode, setMode] = useState('text');
-  const [encoding, setEncoding] = useState('UTF-8');
+  const [mode, setMode] = useState<Mode>('text');
 
   const renderModeComponent = () => {
     switch (mode) {
       case 'text':
-        return <EncodeDecodeText encoding={encoding} />;
+        return <EncodeDecodeText />;
       case 'file':
         return <EncodeDecodeFile />;
       default:
@@ -22,13 +23,20 @@ const App: React.FC = () => {
   return (
     <Container className="mt-5">
       <p className={'h2 mb-4'}>Base64 Encoder/Decoder</p>
-      <FormLabel htmlFor="modeSelect">Mode:</FormLabel>
-      <FormSelect
-        id="modeSelect"
-        value={mode} onChange={e => setMode(e.target.value)}>
-        <option value="text">Text Converter</option>
-        <option value="file">File Converter</option>
-      </FormSelect>
+      <Row>
+        <Col md={2}>
+          <FormLabel htmlFor="modeSelect">Mode</FormLabel>
+        </Col>
+        <Col md={5}>
+          <FormSelect
+            id="modeSelect"
+            value={mode}
+            onChange={e => setMode(e.target.value as Mode)}>
+            <option value="text">Text Converter</option>
+            <option value="file">File Converter</option>
+          </FormSelect>
+        </Col>
+      </Row>
       {renderModeComponent()}
     </Container>
   );
